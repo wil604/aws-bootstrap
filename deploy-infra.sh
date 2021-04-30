fde2716dd6912a487a2ebd844afb5cd78ff910a0
wil604 <wil@wilson-wong.com>
2021-04-30 10:20:00 -0700

    Add a second instance

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -29,26 +29,26 @@ aws cloudformation deploy \
   --parameter-overrides \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
-  # Deploy the CloudFormation template
-  echo -e "\n\n=========== Deploying main.yml ==========="
-  aws cloudformation deploy \
-    --region $REGION \
-    --profile $CLI_PROFILE \
-    --stack-name $STACK_NAME \
-    --template-file main.yml \
-    --no-fail-on-empty-changeset \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --parameter-overrides \
-      EC2InstanceType=$EC2_INSTANCE_TYPE \
-      GitHubOwner=$GH_OWNER \
-      GitHubRepo=$GH_REPO \
-      GitHubBranch=$GH_BRANCH \
-      GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
-      CodePipelineBucket=$CODEPIPELINE_BUCKET
+# Deploy the CloudFormation template
+echo -e "\n\n=========== Deploying main.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file main.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
-    # If the deploy succeeded, show the DNS name of the created instance
+# If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
     --profile awsbootstrap \
-    --query "Exports[?Name=='InstanceEndpoint'].Value"
+    --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
 fi
