c2a249b9e170be5c36097691367b81f21e5062fe
wil604 <wil@wilson-wong.com>
2021-04-30 09:28:05 -0700

    Change Hello World to Hello Cloud

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { hostname } = require('os');
 const http = require('http');
-const message = 'Hello Wil\n';
+const message = 'Hello Cloud Wil\n';
 const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
