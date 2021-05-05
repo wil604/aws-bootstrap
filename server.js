54c2d62deb0f8e21d79a315b78d7fc6be03cb809
wil604 <wil@wilson-wong.com>
2021-05-05 15:42:23 -0700

    Add hostname to hello world message

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { hostname } = require('os');
 const http = require('http');
-const message = 'Hello Cloud Wil\n';
+const message = `Hello World wil from ${hostname()}\n`;
 const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
