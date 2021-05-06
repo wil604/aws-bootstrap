4041fbf1ceb897c07f973f770fb44b0c13d1e1a5
wil604 <wil@wilson-wong.com>
2021-05-05 17:37:51 -0700

    Split out staging nested stack

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,24 +1,23 @@
-#!/bin/bash
-
 STACK_NAME=awsbootstrap6
 REGION=ca-central-1
 CLI_PROFILE=awsbootstrap
-
 EC2_INSTANCE_TYPE=t2.micro
 
-AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
-  --query "Account" --output text`
-CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
-
-# Generate a personal access token with repo and admin:repo_hook
-#    permissions from https://github.com/settings/tokens
 GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
 GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
 GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
 GH_BRANCH=master
 
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+echo $CODEPIPELINE_BUCKET
+
+CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
+echo $CFN_BUCKET
+
+
 # Deploys static resources
-echo -e "\n\n=========== Deploying setup.yml ==========="
+echo "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
@@ -27,15 +26,34 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-    CodePipelineBucket=$CODEPIPELINE_BUCKET
+    CodePipelineBucket=$CODEPIPELINE_BUCKET \
+    CloudFormationBucket=$CFN_BUCKET
+
+# Package up CloudFormation templates into an S3 bucket
+echo "\n\n=========== Packaging main.yml ==========="
+mkdir -p ./cfn_output
+
+PACKAGE_ERR="$(aws cloudformation package \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --template main.yml \
+  --s3-bucket $CFN_BUCKET \
+  --output-template-file ./cfn_output/main.yml 2>&1)"
+
+if ! [[ $PACKAGE_ERR =~ "Successfully packaged artifacts" ]]; then
+  echo "ERROR while running 'aws cloudformation package' command:"
+  echo $PACKAGE_ERR
+  exit 1
+fi
+
 
 # Deploy the CloudFormation template
-echo -e "\n\n=========== Deploying main.yml ==========="
+echo "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
   --stack-name $STACK_NAME \
-  --template-file main.yml \
+  --template-file ./cfn_output/main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
@@ -46,9 +64,9 @@ aws cloudformation deploy \
     GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
-# If the deploy succeeded, show the DNS name of the created instance
-if [ $? -eq 0 ]; then
-  aws cloudformation list-exports \
-    --profile awsbootstrap \
-    --query "Exports[?ends_with(Name,'LBEndpoint')].Value"
+# If the deploy succeeded, show the DNS name of the endpoints
+  if [ $? -eq 0 ]; then
+    aws cloudformation list-exports \
+      --profile awsbootstrap \
+      --query "Exports[?ends_with(Name,'LBEndpoint')].Value"
 fi
